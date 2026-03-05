e86423fad531ac3dc46de6e965eb8295f4c5dde5
Parth Gupta <flourmaa@gmail.com>
2026-03-05 17:56:21 +0000

    fix MongoAsyncModel import

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ from datetime import datetime, timezone, timedelta
 import bcrypt
 import jwt
 from crudadmin import CRUDAdmin
-from fastcrud.mongodb import MongoAsyncModel
+from crudadmin.models import MongoAsyncModel
 
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
